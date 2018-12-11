355a4891aee962515a2b19682a4985034880e936
Sing Mak <mssjsg@gmail.com>
2018-12-11 22:46:30 +0800

    add router

--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React from 'react'
 
 import { render } from 'react-dom'
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-import App from './components/App.jsx'
+import Root from './components/Root'
 import robotQuestApp from './reducers/reducers'
 
-let store = createStore(robotQuestApp)
-let rootElement = document.getElementById('root')
+const store = createStore(robotQuestApp)
 
-render(
-    <Provider store = {store}>
-        <App />
-    </Provider>,
-
-    rootElement
-)
+render(<Root store={store} />, document.getElementById('root'))
